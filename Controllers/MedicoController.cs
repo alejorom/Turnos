d9ff5a121e5b092b8f32c5a2a5d318626fae2023
alejorom <morojela-github@yahoo.com>
2022-02-17 18:27:26 -0500

    Actualizar controlador Medico vs Especialidad

--- Controllers/MedicoController.cs
+++ Controllers/MedicoController.cs
@@ -33,7 +33,9 @@ namespace Turnos.Controllers
             }
 
             var medico = await _context.Medico
-                .FirstOrDefaultAsync(m => m.IdMedico == id);
+                .Where(m => m.IdMedico == id).Include(me => me.MedicoEspecialidad)
+                .ThenInclude(e => e.Especialidad).FirstOrDefaultAsync();
+
             if (medico == null)
             {
                 return NotFound();
@@ -164,6 +166,9 @@ namespace Turnos.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
+            var medicoEspecialidad = await _context.MedicoEspecialidad.FirstOrDefaultAsync(me => me.IdMedico == id);
+            _context.MedicoEspecialidad.Remove(medicoEspecialidad);
+            await _context.SaveChangesAsync();
             var medico = await _context.Medico.FindAsync(id);
             _context.Medico.Remove(medico);
             await _context.SaveChangesAsync();
